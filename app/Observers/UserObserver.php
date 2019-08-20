2f021a57a3172ec3998624b9be6cecc443e20cba
vagrant <vagrant@homestead>
2019-08-20 07:12:51 +0000

    用户注册后默认头像

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -18,4 +18,12 @@ class UserObserver
     {
         //
     }
+
+    public function saving (User $user)
+    {
+        // 只有头像为空时才给设置默认头像
+        if (empty($user->avatar)) {
+            $user->avatar = 'http://dingyue.ws.126.net/e=cCUYs7IAZsGJEQTicVaHw8NpeCO4NEjfzA6S8YOelUI1553444607133.jpg';
+        }
+    }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,12 +12,19 @@ class UsersTableSeeder extends Seeder
 
         // 头像假数据
         $avatars = [
-            'https://cdn.learnku.com/uploads/images/201710/14/1/s5ehp11z6s.png?imageView2/1/w/200/h/200',
-            'https://cdn.learnku.com/uploads/images/201710/14/1/Lhd1SHqu86.png?imageView2/1/w/200/h/200',
-            'https://cdn.learnku.com/uploads/images/201710/14/1/LOnMrqbHJn.png?imageView2/1/w/200/h/200',
-            'https://cdn.learnku.com/uploads/images/201710/14/1/xAuDMxteQy.png?imageView2/1/w/200/h/200',
-            'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200',
-            'https://cdn.learnku.com/uploads/images/201710/14/1/NDnzMutoxX.png?imageView2/1/w/200/h/200',
+            'http://dingyue.ws.126.net/15=9EUhfQyCeBJhGG3SFmAnmIu49kjZ04xYuTwcmI4u=K1553444607528.jpg',
+            'http://dingyue.ws.126.net/e=cCUYs7IAZsGJEQTicVaHw8NpeCO4NEjfzA6S8YOelUI1553444607133.jpg',
+            'http://dingyue.ws.126.net/X9Vf7=bh46tzgtjRww=1iNGPnxq8=pbXdresev11oOlc=1553444608104.jpg',
+            'http://dingyue.ws.126.net/NdTnOc3jFo=3YzhrwbRj0HQlgbXIByICFxRrpxYNWTNPV1553444608915.jpg',
+            'http://dingyue.ws.126.net/ekP90OPTkgyNVkzzwxTFS=rrK2AMqaxxz77oAj0wO2WsN1553444609781.jpg',
+            'http://dingyue.ws.126.net/8yuuu9hpsJfzTkgvWn1VmfvGaEM2U2lmlkyUuaqsf9kDY1553444610272.jpg',
+            'http://dingyue.ws.126.net/bTpMCnYLyeUisl9NLjBlLWpZWmEtMq3569O3dBAomGyHC1553444610764.jpg',
+            'http://dingyue.ws.126.net/TfJ2GsgNtEhVtFHR=vFaYgeIIipSg7rZP3nRzbTag1CZj1553444611227.jpg',
+            'http://dingyue.ws.126.net/AGlTlHnH4du6oEjbc2NenSWEEPSMsPmJpoT0cLIlEvdrO1553444612366.jpg',
+            'http://dingyue.ws.126.net/McsNr7MZNuDfvNY2NT8Hc9B=1u3pducRLMzu7PFFNwjCf1553444612951.jpg',
+            'http://dingyue.ws.126.net/wH1qt243jRWpx08OvIbUUXM=S1Bv54sIMJm0E6Na1NRUm1553444613404.jpg',
+            'http://dingyue.ws.126.net/fuOkyHIqVsDIMnKzg0aES0pO=Lhapf3vDBwyBznK1MFu=1553444614083.jpg',
+            'http://dingyue.ws.126.net/3mk=LfgwLTdUH95sRVaSOaJ9BeD4jT79oTccV3A4KnYqI1553444614487.jpg'
         ];
 
         // 生成数据集合
