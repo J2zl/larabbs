1926ab1211171830c361acb180267dfec90f5d6e
vagrant <vagrant@homestead>
2019-08-24 06:52:03 +0000

    push notification

--- config/app.php
+++ config/app.php
@@ -163,6 +163,8 @@ return [
         Overtrue\LaravelLang\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        App\Providers\EasySmsServiceProvider::class,
+        App\Providers\JpushServiceProvider::class,
 
         /*
          * Package Service Providers...
